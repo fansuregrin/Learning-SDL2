33efb072c35a2f7292be8133671a2e6c54592533
fansuregrin <pwz113436@gmail.com>
2024-06-22 21:42:43 +0800

    Fixed: removed unnecessary lib on linux platform

--- linux.defines.mk
+++ linux.defines.mk
@@ -3,5 +3,5 @@ CFLAGS = -g -fdiagnostics-color=always --std=c++14 -Wall
 MKDIR = mkdir -p
 INCLUDE_PATH = -I/usr/include
 LIB_PATH = -L/usr/lib/x86_64-linux-gnu
-LIB = -lmingw32 -lSDL2main -lSDL2
+LIB = -lSDL2main -lSDL2
 EXE_SUFFIX =
