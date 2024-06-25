3537147915265a92542186c4e935e232b03dd882
fansuregrin <pwz113436@gmail.com>
2024-06-25 14:44:45 +0800

    Updated: windows.defines.mk, linux.defines.mk and Makefile of demo17

--- linux.defines.mk
+++ linux.defines.mk
@@ -1,6 +1,8 @@
 CXX = g++
 CFLAGS = -g -fdiagnostics-color=always --std=c++14 -Wall
 MKDIR = mkdir -p
+RM = rm
+CP = cp
 INCLUDE_PATH = -I/usr/include
 LIB_PATH = -L/usr/lib/x86_64-linux-gnu
 LIB = -lSDL2main -lSDL2
--- windows.defines.mk
+++ windows.defines.mk
@@ -1,8 +1,9 @@
 CXX = g++
 CFLAGS = -g -fdiagnostics-color=always --std=c++14 -Wall
-MSYS_ROOT = C:\msys64
-MKDIR = ${MSYS_ROOT}\usr\bin\mkdir -p
-RM = ${MSYS_ROOT}\usr\bin\rm
+MSYS_ROOT = C:\\msys64
+MKDIR = ${MSYS_ROOT}\\usr\\bin\\mkdir -p
+RM = ${MSYS_ROOT}\\usr\\bin\\rm
+CP = ${MSYS_ROOT}\\usr\\bin\\cp
 INCLUDE_PATH = -I${MSYS_ROOT}\\mingw64\\include
 LIB_PATH = -L${MSYS_ROOT}\\mingw64\\lib
 LIB = -lmingw32 -lSDL2main -lSDL2
